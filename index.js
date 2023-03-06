78a771a6ed432913c6865f325adc47e91726d203
Michael Douglas <maikao0026@gmail.com>
2023-03-06 01:09:19 -0300

    arrumando a lógica

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-document.getElementById("verificar").addEventListener("click", () => {
+document.getElementById("verificar").addEventListener("click", () =>
+{
   const numeroInformado = document.getElementById("numeroInformado").value
   let resultado = calculaSequenciaFibonacci(numeroInformado)
   const respostaTxt = document.getElementById("resultado")
   
-  if (resultado === numeroInformado) 
+  if (resultado == numeroInformado) 
   {
     console.log(`O número ${numeroInformado} pertence à sequência de Fibonacci.`)
     respostaTxt.innerText =`O número ${numeroInformado} pertence à sequência de Fibonacci.`
@@ -14,20 +15,17 @@ document.getElementById("verificar").addEventListener("click", () => {
       `O número ${numeroInformado} não pertence à sequência de Fibonacci.`)
       respostaTxt.innerText = `O número ${numeroInformado} não pertence à sequência de Fibonacci.`;
   }
- 
-
-
 })
 
 function calculaSequenciaFibonacci(num)
 {
-  let a = 0, b = 1, c
-
-  for (let i = 2; i <= num; i++)
+  let a, b, c
+  a=0, b=1
+  do 
   {
-    c = a + b;
-    a = b;
-    b = c;
-  }
-    return c;
+    c = a + b
+    a = b
+    b = c 
+  } while (c<num)
+  return c
 }
